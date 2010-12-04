019b49337178e3ff48df3fe2097846b4f0225e6a
Marcin Nowicki <pr0d1r2@gmail.com>
2010-12-04 11:39:01 +0100

    Add initial rails and postgres.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
-source :gemcutter
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'pg', '0.10.0'
 
 group :development do
   gem 'heroku', '1.14.6'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PolishRemoteItWorkerSurvey::Application.config.secret_token = '8d5cb7f100f70a45110941a22f63a781749c15496133cbfa2752472b0b1c6afd59b59df7c9314a7db1342063e02a02a704e70c989bf0aff1f7b51a11532a0010'
